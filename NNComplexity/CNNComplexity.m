8c574b6f4c9cba1f881878f89aa660d84c03328a
happynear <wf900911@163.com>
2015-11-26 09:46:50 +0800

    complexity

--- NNComplexity/CNNComplexity.m
+++ NNComplexity/CNNComplexity.m
@@ -4,7 +4,7 @@ caffe.set_mode_gpu();
 gpu_id = 0;  % we will use the first gpu in this demo
 caffe.set_device(gpu_id);
 
-net_model = 'D:\project\NNComplexity\net_define.prototxt';
+net_model = 'net_define.prototxt';
 inception_file = 'inception.prototxt';
 inception_content = fileread(inception_file);
 conv1x1_file = '1x1conv.prototxt';
@@ -17,11 +17,11 @@ output_content = fileread(output_file);
 activation = 'ReLU';
 
 layers = {
-%     struct('type', 'convolution', 'outputmaps', 100, 'kernelsize', 3, 'activation', activation) %convolution layer
-%     struct('type', 'convolution', 'outputmaps', 10, 'kernelsize', 3, 'activation', activation) %convolution layer
-%     struct('type', 'convolution', 'outputmaps', 10, 'kernelsize', 3, 'activation', activation) %convolution layer
+    struct('type', 'convolution', 'outputmaps', 100, 'kernelsize', 3, 'activation', activation) %convolution layer
+    struct('type', 'convolution', 'outputmaps', 100, 'kernelsize', 3, 'activation', activation) %convolution layer
+    struct('type', 'convolution', 'outputmaps', 100, 'kernelsize', 3, 'activation', activation) %convolution layer
 %     struct('type', 'pooling', 'scale', 2, 'method', 'AVE')  
-    struct('type', 'inception', 'node1x1', 100, 'reduce3x3', 50, 'node3x3', 100, 'reduce5x5', 50, 'node5x5', 100, 'poolconv', 100) 
+%     struct('type', 'inception', 'node1x1', 100, 'reduce3x3', 50, 'node3x3', 100, 'reduce5x5', 50, 'node5x5', 100, 'poolconv', 100) 
 %     struct('type', 'inception', 'node1x1', 20, 'reduce3x3', 10, 'node3x3', 20, 'reduce5x5', 10, 'node5x5', 20, 'poolconv', 20) 
 %     struct('type', 'inception', 'node1x1', 20, 'reduce3x3', 10, 'node3x3', 20, 'reduce5x5', 10, 'node5x5', 20, 'poolconv', 20) 
 %     struct('type', 'convolution', 'outputmaps', 20, 'kernelsize', 1, 'activation', activation) %convolution layer
